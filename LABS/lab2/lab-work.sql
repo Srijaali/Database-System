52253284d78aebb740f09cd1d22f553993675dd8
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2025-08-28 09:16:59 +0500

    Update lab-work.sql

--- LABS/lab2/lab-work.sql
+++ LABS/lab2/lab-work.sql
@@ -5,3 +5,9 @@ select sum(salary) as totatl_salary from hr.employees;
 select min(salary) as min_salary from hr.employees; 
 select max(salary) as max_salary from hr.employees; 
 select avg(salary) as avg_salary from hr.employees; 
+-- concatenation
+select firsr_name||salary as fn_and_salary from hr.employees;
+SELECT all salary from hr.employees;
+select salary from hr.employees;
+select salary from hr.employees order by(salary) asc;
+select first_name,hire_date from hr.employees order by(first_name) asc;
